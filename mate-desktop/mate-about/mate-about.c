ff81202620fe2401efb513fe545524bb0ba726b1
Perberos <perberos@gmail.com>
2011-10-27 07:36:30 -0300

    にゃあ にゃあ にゃあ

--- mate-desktop/mate-about/mate-about.c
+++ mate-desktop/mate-about/mate-about.c
@@ -1,9 +1,58 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2011 Perberos <perberos@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
 #ifndef __MATE_ABOUT_C__
 #define __MATE_ABOUT_C__
 
 #include "mate-about.h"
 
 // what a mess!
+#ifndef DISABLE_NYANCAT
+
+// Thanks! http://www.gtkforums.com/viewtopic.php?t=1639
+typedef struct _iter_arg {
+	GtkWidget* widget;
+	GdkPixbufAnimation* animation;
+	GdkPixbufAnimationIter* iter;
+} iter_arg_t;
+
+gboolean on_animation_frame(iter_arg_t* object)
+{
+	if (object->widget == NULL)
+	{
+		return FALSE;
+	}
+
+	static gint frame = 1;
+
+	if (gdk_pixbuf_animation_iter_advance(object->iter, NULL))
+	{
+		frame++;
+
+		gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(object->widget), gdk_pixbuf_animation_iter_get_pixbuf(object->iter));
+	}
+
+	return TRUE;
+}
+
+#endif
 
 #if GTK_CHECK_VERSION(3, 0, 0) && !defined(UNIQUE)
 
@@ -50,7 +99,28 @@ void mate_about_run(void)
 	gtk_window_set_default_icon_name(icon);
 
 	// logo
-	#if GTK_CHECK_VERSION(3, 0, 0) || GTK_CHECK_VERSION(2, 6, 0)
+	#ifndef DISABLE_NYANCAT
+		iter_arg_t animation_object;
+		GdkPixbufAnimation* animation;
+		GdkPixbufAnimationIter *iter;
+		GtkWidget* image;
+
+		animation = gdk_pixbuf_animation_new_from_file(PIXMAPS_DIR "nyan-cat.gif", NULL);
+
+		if (animation != NULL)
+		{
+			iter = gdk_pixbuf_animation_get_iter(animation, NULL);
+
+			animation_object.animation = animation;
+			animation_object.iter = iter;
+			animation_object.widget = mate_about_dialog;
+
+			gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(mate_about_dialog), gdk_pixbuf_animation_iter_get_pixbuf(iter));
+
+			g_timeout_add(gdk_pixbuf_animation_iter_get_delay_time(iter), (GSourceFunc) on_animation_frame, (gpointer) &animation_object);
+		}
+
+	#elif GTK_CHECK_VERSION(3, 0, 0) || GTK_CHECK_VERSION(2, 6, 0)
 
 		gtk_about_dialog_set_logo_icon_name(GTK_ABOUT_DIALOG(mate_about_dialog), icon);
 
